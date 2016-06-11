7d921178430f347a8c2cd835647ab9d7b746b3b9
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-06-11 17:08:47 -0400

    Changes to be committed: 	modified:   Build/Intermediates/wgetSafariURL.build/Debug/wgetSafariURL.build/Objects-normal/x86_64/main.dia 	modified:   Build/Intermediates/wgetSafariURL.build/Debug/wgetSafariURL.build/Objects-normal/x86_64/main.o 	modified:   Build/Intermediates/wgetSafariURL.build/Debug/wgetSafariURL.build/dgph 	modified:   Build/Products/Debug/wgetSafariURL.app/Contents/MacOS/wgetSafariURL 	modified:   Build/Products/Debug/wgetSafariURL.app/Contents/Resources/wgetSafariURL.scpt 	new file:   objective c - Execute a terminal command from a Cocoa app - Stack Overflow.pdf 	modified:   wgetSafariURL/main.m

--- wgetSafariURL/main.m
+++ wgetSafariURL/main.m
@@ -1,14 +1,37 @@
-//
-//  main.m
-//  wgetSafariURL
-//
-//  Created by Randy McMillan on 12/30/15.
-//  Copyright © 2015 Randy McMillan. All rights reserved.
-//
+/*
+ * main.m
+ * wgetSafariURL
+ *
+ * Created by Randy McMillan on 12/30/15.
+ * Copyright © 2015 Randy McMillan. All rights reserved.
+ */
 
 #import <Cocoa/Cocoa.h>
 
-int main(int argc, const char *argv[])
+int main(
+  int         argc,
+  const char  *argv[]
+  )
 {
-	return NSApplicationMain(argc, argv);
-}
+
+/*
+ *  int pid = [[NSProcessInfo processInfo] processIdentifier];
+ *  NSPipe *pipe = [NSPipe pipe];
+ *  NSFileHandle *file = pipe.fileHandleForReading;
+ *
+ *  NSTask *task = [[NSTask alloc] init];
+ *  task.launchPath = @"/usr/bin/grep";
+ *  task.arguments = @[@"foo", @"bar.txt"];
+ *  task.standardOutput = pipe;
+ *
+ *  [task launch];
+ *
+ *  NSData *data = [file readDataToEndOfFile];
+ *  [file closeFile];
+ *
+ *  NSString *grepOutput = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+ *  NSLog (@"grep returned:\n%@", grepOutput);
+ */
+
+  return NSApplicationMain(argc, argv);
+} //main
